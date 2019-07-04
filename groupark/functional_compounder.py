18fb0f362f9f73a405a59f45798e985af0f68b57
eecheverry <eecheverry@nubark.com>
2019-07-04 18:47:25 -0500

    Make functional aggregator handle one group

--- groupark/functional_compounder.py
+++ groupark/functional_compounder.py
@@ -1,4 +1,28 @@
+from itertools import groupby
 
 
 class FunctionalCompounder:
-    pass
+
+    def compound(self, groups):
+        def aggregator(data):
+
+            def key_function(item):
+                return tuple(item[group] for group in groups)
+
+            data = sorted(data, key=key_function)
+
+            result = []
+            for group, values in groupby(data, key_function):
+                row = {}
+                operation = 'count'
+
+                composite = sum(1 for _ in values)
+
+                row[f"{groups[0]}"] = group[0]
+                row[f"{operation}_{groups[0]}"] = composite
+
+                result.append(row)
+
+            return result
+
+        return aggregator
--- tests/test_functional_compounder.py
+++ tests/test_functional_compounder.py
@@ -1,6 +1,41 @@
+from pytest import fixture
 from groupark.functional_compounder import FunctionalCompounder
+
+
+@fixture
+def dataset():
+    return [
+        {'name': 'Mazda 3', 'type': 'sedan', 'year': '2018', 'price': 21000},
+        {'name': 'Corolla', 'type': 'sedan', 'year': '2019', 'price': 25000},
+        {'name': 'Tesla M3', 'type': 'sedan', 'year': '2020', 'price': 51000},
+        {'name': 'GMC', 'type': 'truck', 'year': '2019', 'price': 60000},
+        {'name': 'Mercedes', 'type': 'sedan', 'year': '2020', 'price': 70000},
+        {'name': 'Wolkswagen', 'type': 'van', 'year': '2020', 'price': 30000},
+        {'name': 'BMW', 'type': 'sedan', 'year': '2019', 'price': 65000},
+        {'name': 'Ford F150', 'type': 'truck', 'year': '2019', 'price': 70000},
+        {'name': 'Chrysler', 'type': 'van', 'year': '2018', 'price': 45000},
+        {'name': 'Kia', 'type': 'suv', 'year': '2020', 'price': 47000},
+        {'name': 'Jeep', 'type': 'truck', 'year': '2020', 'price': 43000},
+        {'name': 'Audi Q8', 'type': 'suv', 'year': '2019', 'price': 67000}
+    ]
 
 
 def test_functional_compounder_instantiation():
     compounder = FunctionalCompounder()
     assert compounder is not None
+
+
+def test_functional_compounder_compound(dataset):
+    compounder = FunctionalCompounder()
+
+    groups = ['type']
+    aggregator = compounder.compound(groups=groups)
+
+    result = aggregator(dataset)
+
+    assert result == [
+        {'type': 'sedan', 'count_type': 5},
+        {'type': 'suv', 'count_type': 2},
+        {'type': 'truck', 'count_type': 3},
+        {'type': 'van', 'count_type': 2}
+    ]
